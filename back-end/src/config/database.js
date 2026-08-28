86f557d844d98ab112a97b2d655323b5b05c7aaf
wesley.henderson <wesley.henderson@rede.ulbra.br>
2026-08-28 20:41:32 -0300

    Fix for database code.

--- back-end/src/config/database.js
+++ back-end/src/config/database.js
@@ -1,19 +1,24 @@
 import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
-import dns from 'dns';
+// import dns from 'dns';
 
 dotenv.config();
 
-dns.setServers(['8.8.8.8', '8.8.4.4']);
+// dns.setServers(['8.8.8.8', '8.8.4.4']);
+if (!process.env.MONGODB_URI) {
+    console.error("ERRO: MONGODB_URI não está definida no .env!")
+}
 
-const client = new MongoClient(process.env.MONGODB_URI);
+const client = new MongoClient(process.env.MONGODB_URI || '');
 
 export async function connectDB() {
     try {
         await client.connect();
         console.log("Conectado ao MongoDB com sucesso!");
-        return client.db("HSChat");
+        return client.db('HSChat');
     } catch (error) {
-        console.log("Erro ao conectar no MongoDB:", error);
+        console.log('Erro ao conectar no MongoDB:', error);
+        console.error('Erro ao conectar no MongoDB: ', error);
+        throw error;
     }
 }
